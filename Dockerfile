9b035763cf56cb9889fb7d15fd854666e5538eb7
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-04 09:00:51 +0530

    [D75-7] | Raghu K | Terraform Code for RoboShop Project.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM        dokken/centos-8
-RUN         yum install git mysql -y
+COPY        mongo.repo /etc/yum.repos.d/mongo.repo
+RUN         yum install git mysql mongodb-org-shell -y
 COPY        run.sh /
 ENTRYPOINT  ["bash", "/run.sh"]
--- run.sh
+++ run.sh
@@ -2,4 +2,12 @@ source /config/params
 
 git clone https://github.com/raghudevopsb75/${COMPONENT}
 cd $COMPONENT/schema
-mysql -h${DBHOST} -u${DBUSER} -p${DBPASS} <${COMPONENT}.sql
+if [ -n "$DOCDB_ENDPOINT" ]; then
+  curl -L -O /tmp/global-bundle.pem https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+  mongo --ssl --host $DOCDB_ENDPOINT:27017 --sslCAFile /tmp/global-bundle.pem --username $DOCDB_USERNAME --password $DOCDB_PASSWORD <$COMPONENT.js
+fi
+
+if [ -n "$RDS_ENDPOINT" ]; then
+  mysql -h${RDS_ENDPOINT} -u${RDS_USER} -p${RDS_PASS} <${COMPONENT}.sql
+fi
+
