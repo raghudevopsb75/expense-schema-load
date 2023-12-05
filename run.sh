b8f30aa41405a1bff179dbf3e320fee6ce03e82e
Sriharsha Kalluru <sriharsha@thinkdeeply.com>
2023-12-05 07:54:29 +0530

    [D75-7] | Raghu K | Terraform Code for RoboShop Project.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ source /config/params
 git clone https://github.com/raghudevopsb75/${COMPONENT}
 cd $COMPONENT/schema
 if [ -n "$DOCDB_ENDPOINT" ]; then
-  curl -L -O /tmp/global-bundle.pem https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+  curl -L -o /tmp/global-bundle.pem https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
   mongo --ssl --host $DOCDB_ENDPOINT:27017 --sslCAFile /tmp/global-bundle.pem --username $DOCDB_USERNAME --password $DOCDB_PASSWORD <$COMPONENT.js
 fi
 
